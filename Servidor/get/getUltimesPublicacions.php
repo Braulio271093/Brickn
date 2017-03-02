75b943ecd4ff3b96b0b2d8f9b7271a5453e92501
pau <pau@dinantia.com>
2017-03-02 15:37:09 +0100

    get Ultimes publicacions

--- Servidor/get/getUltimesPublicacions.php
+++ Servidor/get/getUltimesPublicacions.php
@@ -4,8 +4,31 @@
 
     $idUsuari = $_GET['idUsuari'];
 
-    $sql = "SELECT usuari.nomUsuari, grup.nom, publicacio.publicacio, publicacio.dataPublicacio, publicacio.tipus
-            FROM publicacio JOIN usuari_grup ON usuari_grup.idUsuari = $idUsuari JOIN usuari ON usuari.id = publicacio.idUsuari
-            JOIN grup ON grup.id = publicacio.idGrup ORDER B";
+    $sql = "SELECT usuari.nomUsuari, grup.nom, publicacio.publicacio FROM publicacio 
+            JOIN usuari ON publicacio.idUsuari = usuari.id 
+            JOIN grup ON grup.id = publicacio.idGrup 
+            WHERE publicacio.dataPublicacio IN (SELECT max(dataPublicacio) FROM publicacio WHERE publicacio.idGrup = idGrup GROUP BY idGrup) 
+            AND publicacio.idUsuari != $idUsuari
+            ORDER BY publicacio.dataPublicacio DESC";
+    
+    $result = mysqli_query($conn, $sql);
+    $res = []; //publicacions
+    $i = 0;
+
+	if (mysqli_num_rows($result) > 0) {
+		while($row = mysqli_fetch_row($result)) {
+            $nomPublicador = $row[0];
+            $nomGrup = $row[1];
+            $publicacio = $row[2];
+            $res[$i] = ["nomPublicador" => $nomPublicador, "nomGrup" => $nomGrup, "publicacio" => $publicacio];
+            $i++;
+        }
+        echo json_encode($res);
+	}
+	else {
+		echo 0;
+	}
+	mysqli_close($conn);
+
 
 ?>
