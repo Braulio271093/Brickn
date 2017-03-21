9b9a7a85bed9242ddd7fb3bc149022d1afbe3ff4
test <test@test.com>
2017-03-21 13:53:35 +0100

    Events

---
+++ Servidor/get/getUltimEvent.php
@@ -0,0 +1,25 @@
+<?php
+    require '../connectDB.php';
+	header("Access-Control-Allow-Origin: *");
+
+    $idGrup = $_GET['idGrup'];
+
+    $sql = "SELECT event.id, usuari.nomUsuari, usuari.fotoPerfil, 
+                   grup_publicacions.dataPublicacio, event.nom, event.descripcio,
+                   event.dateStart, event.dateEnd
+            FROM grup_publicacions 
+            JOIN usuari ON grup_publicacions.idUsuari = usuari.id
+            JOIN event ON grup_publicacions.id = event.id
+            WHERE grup_publicacions.idGrup = $idGrup
+            ORDER BY grup_publicacions.dataPublicacio DESC LIMIT 1";
+   $result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($result) > 0) {
+		$row = mysqli_fetch_row($result);
+        $publicacio = ["id" => $row[0], "publicador" => $row[1], "imgPublicador" => $row[2], 
+                       "dataPublicacio" => $row[3], "nomEvent" => $row[4], 
+                       "descripcioEvent" => $row[5], "dateStart" => $row[6],
+                       "dateEnd" => $row[7]];
+        echo json_encode($publicacio);
+    }
+    mysqli_close($conn);
+?>
