5f4d17df6c298d91faf1f9f10e9d8e01be4f928f
test <test@test.com>
2017-04-21 11:31:10 +0200

    Millores

--- Servidor/get/getEventsUsuari.php
+++ Servidor/get/getEventsUsuari.php
@@ -3,7 +3,7 @@
 	header("Access-Control-Allow-Origin: *");
     $idUsari = $_GET['idUsuari'];
 
-    $sql = "SELECT event.id, coordX, coordY FROM event
+    $sql = "SELECT event.id, coordX, coordY, icon FROM event
             JOIN grup_publicacions ON grup_publicacions.id = event.id 
             WHERE idUsuari = $idUsari";
     
@@ -13,7 +13,7 @@
 
 	if (mysqli_num_rows($result) > 0) {
 		while($row = mysqli_fetch_row($result)) {
-            $res[$i] = ["id" => $row[0], "coordX" => $row[1], "coordY" => $row[2]];
+            $res[$i] = ["id" => $row[0], "coordX" => $row[1], "coordY" => $row[2], "icon" => $row[3]];
             $i++;
         }
         echo json_encode($res);
